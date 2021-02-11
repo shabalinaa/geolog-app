fc40112742733db2ac35e7ce0a1f2eb628fe26a0
shabalinaa <shabalina.anastasiya@gmail.com>
2021-02-11 16:59:17 +0400

    изменение настроект приложения

--- geolog-app/settings.py
+++ geolog-app/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, '/static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
